0665c3f8a6cfe815a749308d18c144a198519dea
lekejosh <lekejosh6wf@gmail.com>
2023-04-22 20:05:02 +0100

    commit

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ io.on("connection", (socket) => {
   socket.on("join chat", (room) => {
     socket.join(room);
     console.log("User Joined Room" + room);
-  });
+  }); 
 
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
