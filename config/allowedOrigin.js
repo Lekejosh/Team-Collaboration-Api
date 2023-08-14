0896a37fe0ea1253a50947268c16d97f90141ff0
lekejosh <lekejosh6wf@gmail.com>
2023-08-14 12:05:22 +0100

    add: allowed origin

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,5 +1,5 @@
 const allowedOrigin = [
-  "http://localhost:4000",
+  "https://master--magenta-shortbread-105779.netlify.app/",
   "http://localhost:8080",
   "https://64da06282b3e793092c3c767--magenta-shortbread-105779.netlify.app/",
   "https://master--magenta-shortbread-105779.netlify.app/",
