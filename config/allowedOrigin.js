20f53eb31b3a3cb3ef73f787a316218f0bada297
lekejosh <lekejosh6wf@gmail.com>
2023-08-14 11:56:04 +0100

    add: allowed origin

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,6 +2,7 @@ const allowedOrigin = [
   "http://localhost:4000",
   "http://localhost:8080",
   "https://64da06282b3e793092c3c767--magenta-shortbread-105779.netlify.app/",
+  "https://master--magenta-shortbread-105779.netlify.app/",
 ];
 
 module.exports = allowedOrigin;
