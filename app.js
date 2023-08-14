84db7013b74c755d3500a3a6dee62672cfe5a6fd
lekejosh <lekejosh6wf@gmail.com>
2023-08-14 12:52:46 +0100

    cors wahala

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
   // res.setHeader(
   //   "Access-Control-Allow-Origin",
   //   "https://master--magenta-shortbread-105779.netlify.app/"
-  );
+  // );
 
   next();
 });
