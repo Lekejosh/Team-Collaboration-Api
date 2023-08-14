e8581532fdbb789e757138704fdaed97a6bce406
lekejosh <lekejosh6wf@gmail.com>
2023-08-14 12:51:58 +0100

    cors wahala

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const treblle = require("@treblle/express");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 
-app.enable("trust proxy");
+app.enable('trust proxy');
 app.use(
   session({
     secret: "egeGBTCTEcgwrtgc54cg66666666h.b/3/3.b/[g[er2",
@@ -60,7 +60,10 @@ app.use((req, res, next) => {
   res.setHeader("Content-Type", "application/json");
 
   // Access-Control-Allow-Origin
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader(
+  //   "Access-Control-Allow-Origin",
+  //   "https://master--magenta-shortbread-105779.netlify.app/"
+  );
 
   next();
 });
